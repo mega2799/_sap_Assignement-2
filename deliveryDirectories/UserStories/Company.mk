acba17bf0b680a19388b7b430e277dfb632d75d1
『 Matteo Santoro 』 <65976958+mega2799@users.noreply.github.com>
2023-11-12 15:51:56 +0000

    + users storiers

---
+++ deliveryDirectories/UserStories/Company.mk
@@ -0,0 +1,8 @@
+# Company 
+
+
+Come compagnia di innovazione vogliamo offrire un nuovo servizio chiamato EScooters per poter offrire la possibilita' di spostarsi alle persone che non hanno mezzi necessari o che intendono spostarsi in zone proibite al traffico.
+
+
+Si dispone di un numero elevato di mezzi di trasporto elettrici che verranno collocati in particolari zone delle citta' adibite alla loro ricarica al loro noleggio. Questo non deve richiedere nessun intervento di operatori e deve essere svolto interamente sull'applicazione che verra' sviluppata per l'interazione tra clienti e mezzi, a cui i clienti dovranno registrarsi e avranno la possibilita' di noleggiare i mezzi e poterli anche prenotare.
+L'interazione tra cliente e mezzo deve essere monitorata per poter eventualmente risalire a mancanze di rispetto delle regole nell'utilizzo e dei loro responsabili. A questo scopo servira' inoltre una dashboard visiva per poter monitorare e l'utilizzo dele apparecchiature ma anche il loro stato di utlizzo, se questi mezzi necessitassero di riparazioni il compito della dashboard sara' quello di segnalarlo e quindi un terzo si occupera' delle opere di riparazione e/o mantenimento consultando quindi questa dashboard.
---
+++ deliveryDirectories/UserStories/User.mk
@@ -0,0 +1,10 @@
+# User 
+
+## Matteo 
+
+Come utente dell'applicazione `EScooters` voglio poter usufruire di un servizio veloce, economico per potermi spostare liberamente in citta' a qualsiasi ora del giorno.
+
+Sono uno studente universitario, quindi so perfettamente come usare uno smartphone ed interagire con strumenti smart, vorrei un applicazione che mi permatta di poter affitare per breve periodo di scooter elettrici.
+Non dispongo di altri mezzi di trasporto quindi e' fondamentale poter trovare gli scooter in giro per la citta' entro un raggio breve da ovunque io mi trovi, attraverso una mappa potrei scegliere il mezzo piu' vicino a me e noleggiarlo per poi poterlo lasciare entro la propria piazzola di ricarica. 
+
+Alcune volte potrei avere bisogno di assicurarmi di poter utilizzare un mezzo per importanti impegni (esami) quindi questa applicazione deve permettermi di poter prenotare il mezzo per un tempo limitato e inoltre di poter sapere chi ha utilizzato lo scooter prima di me dato che potrebbe averlo danneggiato.
